8a3da44d797fc10f7568a4b9004b92d9fd647285
13tom13 <13tom13@mail.ru>
2023-07-12 00:38:19 +0200

    implemented Testcontainers

--- src/test/java/ru/netology/moneytransferservice/MoneytransferserviceApplicationTests.java
+++ src/test/java/ru/netology/moneytransferservice/MoneytransferserviceApplicationTests.java
@@ -1,10 +1,16 @@
 package ru.netology.moneytransferservice;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.netology.moneytransferservice.controller.TransferController;
 import ru.netology.moneytransferservice.exceptions.ErrorConfirmation;
 import ru.netology.moneytransferservice.model.Amount;
@@ -12,23 +18,55 @@ import ru.netology.moneytransferservice.model.ConfirmOperation;
 import ru.netology.moneytransferservice.model.OperationId;
 import ru.netology.moneytransferservice.model.Transfer;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MoneytransferserviceApplicationTests {
 
     @Autowired
     private TransferController transferController;
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    static Transfer transferTest;
+
+    @Container
+    private static final GenericContainer<?> moneytransferservice = new GenericContainer<>("moneytransferservice:latest")
+            .withExposedPorts(5500);
+
+    @BeforeEach
+    public void setUp() {
+        transferTest = Mockito.mock(Transfer.class);
+        Amount amountTest = Mockito.mock(Amount.class);
+        Mockito.when(amountTest.value()).thenReturn(100);
+        Mockito.when(amountTest.currency()).thenReturn("RUR");
+        Mockito.when(transferTest.amount()).thenReturn(amountTest);
+        Mockito.when(transferTest.cardFromNumber()).thenReturn("0000000000000000");
+        Mockito.when(transferTest.cardToNumber()).thenReturn("1000000000000000");
+        Mockito.when(transferTest.cardFromCVV()).thenReturn("123");
+        Mockito.when(transferTest.cardFromValidTill()).thenReturn("1234");
+    }
+
+    @AfterEach
+    public void setDown() {
+        transferTest = null;
+    }
+
+    @Test
+    void moneytransferserviceTest() {
+        // given:
+        moneytransferservice.start();
+        Integer port = moneytransferservice.getMappedPort(5500);
+        // when:
+        OperationId operationId = restTemplate.postForObject
+                ("http://localhost:" + port + "/transfer", transferTest, OperationId.class);
+        // then:
+        Assertions.assertNotNull(operationId.operationId());
+    }
+
 
     @Test
     void transferControllerTest() {
-        // given:
-        Transfer transferTest = Mockito.mock(Transfer.class);
-        Amount amountTest = Mockito.mock(Amount.class);
-        Mockito.when(amountTest.value()).thenReturn(1000);
-        Mockito.when(transferTest.amount()).thenReturn(amountTest);
-        Mockito.when(transferTest.cardFromNumber()).thenReturn("0000000000000000");
-
-        // when:
         OperationId testID = transferController.transfer(transferTest);
         // then:
         Assertions.assertNotNull(testID);
@@ -38,15 +76,17 @@ class MoneytransferserviceApplicationTests {
     void confirmOperationTest() {
         // given:
         String testIDRequest = "1234";
+        String testCode = "0000";
         ConfirmOperation confirmOperation = Mockito.mock(ConfirmOperation.class);
-        Mockito.when(confirmOperation.code()).thenReturn("0000");
+        Mockito.when(confirmOperation.code()).thenReturn(testCode);
         Mockito.when(confirmOperation.operationId()).thenReturn(testIDRequest);
-
         // when:
         ErrorConfirmation testConfirmationException = Assertions.assertThrows(ErrorConfirmation.class,
                 () -> transferController.confirmOperation(confirmOperation));
         // then:
-        Assertions.assertEquals("The transaction has not been approved", testConfirmationException.getMessage());
+        Assertions.assertEquals("The transaction has not been approved",
+                testConfirmationException.getMessage());
     }
+
 
 }
